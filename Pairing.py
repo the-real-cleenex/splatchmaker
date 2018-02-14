b1ebcec4f34d2066dc10ee44567b4477d745026e
the-real-cleenex <chris@cleenex.xyz>
2018-02-13 16:13:59 -0800

    Added basic map+mode generation.
    Basic support added for generating valid map and mode combinations on the basis of TO preferences.

--- Pairing.py
+++ Pairing.py
@@ -1,8 +1,29 @@
 import json
 from pprint import pprint
 
-mapList = json.load(open("maps.json"))
-toPreferences = json.load(open("toPreferences.json"))
+def main():
+    mapList = json.load(open("jsons\\maps.json"))["data"]
+    toPreferences = json.load(open("jsons\\toPreferences.json"))["data"]
 
+    # Parsing preferences out of toPreferences.json.
+    tournamentID = toPreferences["tournamentID"]
+    bannedModes = toPreferences["bannedModes"]
+    bannedMaps = toPreferences["bannedMaps"]
 
+    validMapPool = []
 
+    # Load valid maps, striking maps in the ban list.
+    for stage in mapList:
+        if stage["name"] not in bannedMaps:
+            validMapPool.append(stage)
+
+    validMapModePool = []
+
+    for validMap in validMapPool:
+        for proposedMode in validMap["allowedModes"]:
+            if proposedMode not in bannedModes:
+                validMapModePool.append("{0} on {1}".format(proposedMode, validMap["name"]))
+
+    pprint(validMapModePool)
+
+main()
